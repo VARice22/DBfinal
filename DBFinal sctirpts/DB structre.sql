285ac23106b17593dff661eb7e621b068b57a739
Aaron V <38258274+VARice22@users.noreply.github.com>
2021-12-04 19:37:57 -0500

    Secondary updates to population

--- DBFinal sctirpts/DB structre.sql
+++ DBFinal sctirpts/DB structre.sql
@@ -9,3 +9,13 @@ CREATE TABLE javamdb.song (
   genre VARCHAR(255),
   PRIMARY KEY (file_name))
 ;
+
+CREATE TABLE javamdb.song (
+  file_name VARCHAR(255) NOT NULL,
+  title VARCHAR(255),
+  artist VARCHAR(255),
+  album VARCHAR(255),
+  tracknum VARCHAR(255),
+  genre VARCHAR(255),
+  PRIMARY KEY (file_name))
+;
--- DBFinal sctirpts/DBpopulate.sql
+++ DBFinal sctirpts/DBpopulate.sql
@@ -1,14 +1,53 @@
 USE javamdb;
 
 INSERT INTO song (file_name, title, artist, album, tracknum, genre) 
-VALUES('Back in Black.mp3', 'Back in Black', 'AC/DC', 'THIS ONE!', '13', 'Hard Rock');/*79-iD3 spec*/
+VALUES('Back in Black.mp3', 'Back in Black', 'AC/DC', 'THIS ONE!', '13', 'Hard Rock');
 
 INSERT INTO song (file_name, title, artist, album, tracknum, genre) 
-VALUES('01 長い長い夢の中の宴 ～バッカーノ! のテーマ.mp3', 'Banquet Inside the Long Long Dream', 'Makoto Yoshimori', 'BACCANO! ORIGINAL SOUNDTRACK SPIRAL MELODIES', '1', 'Jazz');/*08-iD3 spec*/
+VALUES('01 長い長い夢の中の宴 ～バッカーノ! のテーマ.mp3', 'Banquet Inside the Long Long Dream', 'Makoto Yoshimori', 'BACCANO! ORIGINAL SOUNDTRACK SPIRAL MELODIES', '1', 'Jazz');
 
 INSERT INTO song (file_name, title, artist, album, tracknum, genre) 
-VALUES('13 ローリンガール [Live].mp3', "Rollin' Girl [Live]", 'hitorie', '4', '27', 'Rock');/*17-iD3 spec*/
+VALUES('13 ローリンガール [Live].mp3', "Rollin' Girl [Live]", 'hitorie', '4', '27', 'Rock');
 
 INSERT INTO song (file_name, title, artist, album, tracknum, genre) 
-VALUES("Rollin' Girl.mp3", "Rollin' Girl", 'wowaka', 'VOCALOID', '91', 'Rock');/*17-iD3 spec*/
+VALUES("Rollin' Girl.mp3", "Rollin' Girl", 'wowaka', 'VOCALOID', '91', 'Rock');
 
+INSERT INTO song (file_name, title, artist, album, tracknum, genre) 
+VALUES("Rollin' Girl.mp3", "Rollin' Girl", 'wowaka', 'VOCALOID', '91', 'Rock');
+
+INSERT INTO song (file_name, title, artist, album, tracknum, genre) 
+VALUES('01 Return to Zero.mp3', 'Return to Zero', 'Fear, and Loathing in Las Vegas', 'New Sunrise', '1', 'Metal');
+INSERT INTO song (file_name, title, artist, album, tracknum, genre) 
+VALUES('02 Before I Fail.mp3', 'Before I Fail', 'Fear, and Loathing in Las Vegas', 'New Sunrise', '2', 'Metal');
+INSERT INTO song (file_name, title, artist, album, tracknum, genre) 
+VALUES('03 Power of Life and Death.mp3', 'Power of Life and Death', 'Fear, and Loathing in Las Vegas', 'New Sunrise', '3', 'Metal');
+INSERT INTO song (file_name, title, artist, album, tracknum, genre) 
+VALUES('04 LLLD.mp3', 'LLLD', 'Fear, and Loathing in Las Vegas', 'New Sunrise', '4', 'Metal');
+INSERT INTO song (file_name, title, artist, album, tracknum, genre) 
+VALUES('05 To Catch the Right Way.mp3', 'To Catch the Right Way', 'Fear, and Loathing in Las Vegas', 'New Sunrise', '5', 'Metal');
+INSERT INTO song (file_name, title, artist, album, tracknum, genre) 
+VALUES('06 Interlude.mp3', 'Interlude', ' Fear, and Loathing in Las Vegas', 'New Sunrise', '6', 'Metal');
+INSERT INTO song (file_name, title, artist, album, tracknum, genre) 
+VALUES('07 SHINE.mp3', 'SHINE', 'Fear, and Loathing in Las Vegas', 'New Sunrise', '7', 'Metal');
+INSERT INTO song (file_name, title, artist, album, tracknum, genre) 
+VALUES('08 Set Your Goal.mp3', 'Set Your Goal', 'Fear, and Loathing in Las Vegas', 'New Sunrise', '8', 'Metal');
+INSERT INTO song (file_name, title, artist, album, tracknum, genre) 
+VALUES('09 Accept Each Other’s Sense of Values.mp3', 'Accept Each Other’s Sense of Values', 'Fear, and Loathing in Las Vegas', 'New Sunrise', '9', 'Metal');
+INSERT INTO song (file_name, title, artist, album, tracknum, genre) 
+VALUES('10 Fight Against the Limit.mp3', 'Fight Against the Limit', 'Fear, and Loathing in Las Vegas', 'New Sunrise', '10', 'Metal');
+INSERT INTO song (file_name, title, artist, album, tracknum, genre) 
+VALUES('11 The Sun Also Rises.mp3', 'The Sun Also Rises', 'Fear, and Loathing in Las Vegas', 'New Sunrise', '11', 'Metal');
+
+INSERT INTO song (file_name, title, artist, album, tracknum, genre) 
+VALUES('01 SHINE.mp3', 'SHINE', 'Fear, and Loathing in Las Vegas', 'SHINE', '1', 'Metal');
+INSERT INTO song (file_name, title, artist, album, tracknum, genre) 
+VALUES('02 Something to Gain After the Pain.mp3', 'Something to Gain After the Pain', 'Fear, and Loathing in Las Vegas', 'SHINE', '2', 'Metal');
+
+INSERT INTO song (file_name, title, artist, album, tracknum, genre) 
+VALUES('01 Funky Kitchen.mp3', 'Funky Kitchen', 'BRADIO', 'YES', '1', 'Funk');
+INSERT INTO song (file_name, title, artist, album, tracknum, genre) 
+VALUES('02 スキャット・ビート.mp3', 'Scat Beat', 'BRADIO', 'YES', '2', 'Funk');
+INSERT INTO song (file_name, title, artist, album, tracknum, genre) 
+VALUES('03 Sexy Lover.mp3', 'Sexy Lover', 'BRADIO', 'YES', '3', 'Funk');
+INSERT INTO song (file_name, title, artist, album, tracknum, genre) 
+VALUES('Flyers.mp3', 'Flyers', 'BRADIO', 'THIS ONE!', '37', 'Funk');
